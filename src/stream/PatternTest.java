b5335e595a3acc171e8b02f49501de9fac8dd6d2
wangf <810833939@qq.com>
2016-02-02 14:19:27 +0800

    kkkkk

--- src/stream/PatternTest.java
+++ src/stream/PatternTest.java
@@ -1,10 +1,12 @@
 package stream;
 
+import java.util.LinkedHashMap;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class PatternTest {
 	public static void main(String[] args) {
-		String tt = "aa;bb;cc";
-		Pattern.compile(";").splitAsStream(tt).forEach(System.out::println);
+		String tt = "aa:11;bb:22;cc:33";
+		Pattern.compile(";").splitAsStream(tt).collect(Collectors.toMap(x->x.split(":")[0],x->x.split(":")[1],(x,y)->{throw new IllegalStateException("");},LinkedHashMap::new)).forEach((x,y)->System.out.println(x+"--"+y));
 	}
 }
